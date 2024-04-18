f4a3904e990ff6d9eb5a140a2b155aea54867d58
James Baker <jlbaker361@gmail.com>
2024-04-17 21:12:43 -0400

    removed nsfw and fixed text embedding

---
+++ inference.py
@@ -0,0 +1,229 @@
+from diffusers import StableDiffusionPipeline
+from diffusers.pipelines.stable_diffusion.pipeline_output import StableDiffusionPipelineOutput
+from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion import retrieve_timesteps, rescale_noise_cfg
+from diffusers.image_processor import PipelineImageInput
+from typing import Any, Callable, Dict, List, Optional, Union
+import torch
+
+def call_vanilla_with_dict(pipeline: StableDiffusionPipeline,
+        prompt: str = None,
+        height: Optional[int] = None,
+        width: Optional[int] = None,
+        num_inference_steps: int = 50,
+        timesteps: List[int] = None,
+        guidance_scale: float = 7.5,
+        negative_prompt: str = "",
+        num_images_per_prompt: Optional[int] = 1,
+        eta: float = 0.0,
+        generator: Optional[Union[torch.Generator, List[torch.Generator]]] = None,
+        latents: Optional[torch.FloatTensor] = None,
+        prompt_embeds: Optional[torch.FloatTensor] = None,
+        negative_prompt_embeds: Optional[torch.FloatTensor] = None,
+        ip_adapter_image: Optional[PipelineImageInput] = None,
+        ip_adapter_image_embeds: Optional[List[torch.FloatTensor]] = None,
+        output_type: Optional[str] = "pil",
+        cross_attention_kwargs: Optional[Dict[str, Any]] = None,
+        guidance_rescale: float = 0.0,
+        clip_skip: Optional[int] = None,
+        callback_on_step_end: Optional[Callable[[int, int, Dict], None]] = None,
+        callback_on_step_end_tensor_inputs: List[str] = ["latents"],
+        token_dict:dict=None,
+        cfg_dict:dict={},
+        **kwargs,):
+    
+    callback = kwargs.pop("callback", None)
+    callback_steps = kwargs.pop("callback_steps", None)
+
+    # 0. Default height and width to unet
+    height = height or pipeline.unet.config.sample_size * pipeline.vae_scale_factor
+    width = width or pipeline.unet.config.sample_size * pipeline.vae_scale_factor
+    # to deal with lora scaling and other possible forward hooks
+
+    # 1. Check inputs. Raise error if not correct
+    pipeline.check_inputs(
+        prompt,
+        height,
+        width,
+        callback_steps,
+        negative_prompt,
+        prompt_embeds,
+        negative_prompt_embeds,
+        ip_adapter_image,
+        ip_adapter_image_embeds,
+        callback_on_step_end_tensor_inputs,
+    )
+
+    pipeline._guidance_scale = guidance_scale
+    pipeline._guidance_rescale = guidance_rescale
+    pipeline._clip_skip = clip_skip
+    pipeline._cross_attention_kwargs = cross_attention_kwargs
+    pipeline._interrupt = False
+
+    # 2. Define call parameters
+    if prompt is not None and isinstance(prompt, str):
+        batch_size = 1
+    elif prompt is not None and isinstance(prompt, list):
+        batch_size = len(prompt)
+    else:
+        batch_size = prompt_embeds.shape[0]
+
+    device = pipeline._execution_device
+
+    timesteps, num_inference_steps = retrieve_timesteps(pipeline.scheduler, num_inference_steps, device, timesteps)
+
+    # 3. Encode input prompt
+    lora_scale = (
+        pipeline.cross_attention_kwargs.get("scale", None) if pipeline.cross_attention_kwargs is not None else None
+    )
+
+    prompt_dict={}
+    negative_input_ids=pipeline.tokenizer(
+                    negative_prompt,
+                    padding="max_length",
+                    truncation=True,
+                    max_length=pipeline.tokenizer.model_max_length,
+                    return_tensors="pt",
+                ).input_ids.to(device=pipeline.device)
+    negative_prompt_embeds=pipeline.text_encoder(negative_input_ids)[0]
+    if token_dict is None:
+        input_ids=pipeline.tokenizer(
+                    prompt,
+                    padding="max_length",
+                    truncation=True,
+                    max_length=pipeline.tokenizer.model_max_length,
+                    return_tensors="pt",
+                ).input_ids.to(device=pipeline.device)
+        prompt_embeds=pipeline.text_encoder(input_ids)[0]
+        if pipeline.do_classifier_free_guidance:
+            prompt_embeds = torch.cat([negative_prompt_embeds, prompt_embeds])
+    else:
+        for timestep_key,token in token_dict.items():
+            input_ids=pipeline.tokenizer(
+                    prompt.format(token),
+                    padding="max_length",
+                    truncation=True,
+                    max_length=pipeline.tokenizer.model_max_length,
+                    return_tensors="pt",
+                ).input_ids.to(device=pipeline.device)
+            prompt_embeds=pipeline.text_encoder(input_ids)[0]
+            if pipeline.do_classifier_free_guidance:
+                prompt_embeds = torch.cat([negative_prompt_embeds, prompt_embeds])
+            print(timestep_key)
+            prompt_dict[timestep_key]=prompt_embeds
+
+    # For classifier free guidance, we need to do two forward passes.
+    # Here we concatenate the unconditional and text embeddings into a single batch
+    # to avoid doing two forward passes
+
+    if ip_adapter_image is not None or ip_adapter_image_embeds is not None:
+        image_embeds = pipeline.prepare_ip_adapter_image_embeds(
+            ip_adapter_image,
+            ip_adapter_image_embeds,
+            device,
+            batch_size * num_images_per_prompt,
+            pipeline.do_classifier_free_guidance,
+        )
+
+    # 5. Prepare latent variables
+    num_channels_latents = pipeline.unet.config.in_channels
+    latents = pipeline.prepare_latents(
+        batch_size * num_images_per_prompt,
+        num_channels_latents,
+        height,
+        width,
+        prompt_embeds.dtype,
+        device,
+        generator,
+        latents,
+    )
+
+    # 6. Prepare extra step kwargs. TODO: Logic should ideally just be moved out of the pipeline
+    extra_step_kwargs = pipeline.prepare_extra_step_kwargs(generator, eta)
+
+    # 6.1 Add image embeds for IP-Adapter
+    added_cond_kwargs = (
+        {"image_embeds": image_embeds}
+        if (ip_adapter_image is not None or ip_adapter_image_embeds is not None)
+        else None
+    )
+
+    # 6.2 Optionally get Guidance Scale Embedding
+    timestep_cond = None
+    if pipeline.unet.config.time_cond_proj_dim is not None:
+        guidance_scale_tensor = torch.tensor(pipeline.guidance_scale - 1).repeat(batch_size * num_images_per_prompt)
+        timestep_cond = pipeline.get_guidance_scale_embedding(
+            guidance_scale_tensor, embedding_dim=pipeline.unet.config.time_cond_proj_dim
+        ).to(device=device, dtype=latents.dtype)
+
+    # 7. Denoising loop
+    num_warmup_steps = len(timesteps) - num_inference_steps * pipeline.scheduler.order
+    pipeline._num_timesteps = len(timesteps)
+    with pipeline.progress_bar(total=num_inference_steps) as progress_bar:
+        for i, t in enumerate(timesteps):
+            if pipeline.interrupt:
+                continue
+
+            # expand the latents if we are doing classifier free guidance
+            latent_model_input = torch.cat([latents] * 2) if pipeline.do_classifier_free_guidance else latents
+            latent_model_input = pipeline.scheduler.scale_model_input(latent_model_input, t)
+            if prompt_dict is not None:
+                print(t.long().detach().tolist())
+                prompt_embeds=prompt_dict[t.long().detach().tolist()]
+            # predict the noise residual
+            noise_pred = pipeline.unet(
+                latent_model_input,
+                t,
+                encoder_hidden_states=prompt_embeds,
+                timestep_cond=timestep_cond,
+                cross_attention_kwargs=pipeline.cross_attention_kwargs,
+                added_cond_kwargs=added_cond_kwargs,
+                return_dict=False,
+            )[0]
+
+            # perform guidance
+            if pipeline.do_classifier_free_guidance:
+                noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+                noise_pred = noise_pred_uncond + pipeline.guidance_scale * (noise_pred_text - noise_pred_uncond)
+
+            if pipeline.do_classifier_free_guidance and t.long() in cfg_dict:
+                # Based on 3.4. in https://arxiv.org/pdf/2305.08891.pdf
+                noise_pred = rescale_noise_cfg(noise_pred, noise_pred_text, guidance_rescale=cfg_dict[t.long()])
+            elif pipeline.do_classifier_free_guidance and pipeline.guidance_rescale > 0.0:
+                noise_pred = rescale_noise_cfg(noise_pred, noise_pred_text, guidance_rescale=pipeline.guidance_rescale)
+
+            # compute the previous noisy sample x_t -> x_t-1
+            latents = pipeline.scheduler.step(noise_pred, t, latents, **extra_step_kwargs, return_dict=False)[0]
+
+            if callback_on_step_end is not None:
+                callback_kwargs = {}
+                for k in callback_on_step_end_tensor_inputs:
+                    callback_kwargs[k] = locals()[k]
+                callback_outputs = callback_on_step_end(pipeline, i, t, callback_kwargs)
+
+                latents = callback_outputs.pop("latents", latents)
+                prompt_embeds = callback_outputs.pop("prompt_embeds", prompt_embeds)
+                negative_prompt_embeds = callback_outputs.pop("negative_prompt_embeds", negative_prompt_embeds)
+
+            # call the callback, if provided
+            if i == len(timesteps) - 1 or ((i + 1) > num_warmup_steps and (i + 1) % pipeline.scheduler.order == 0):
+                progress_bar.update()
+                if callback is not None and i % callback_steps == 0:
+                    step_idx = i // getattr(pipeline.scheduler, "order", 1)
+                    callback(step_idx, t, latents)
+
+    if not output_type == "latent":
+        image = pipeline.vae.decode(latents / pipeline.vae.config.scaling_factor, return_dict=False, generator=generator)[
+            0
+        ]
+    else:
+        image = latents
+        has_nsfw_concept = None
+
+    do_denormalize = [True] * image.shape[0]
+
+    image = pipeline.image_processor.postprocess(image, output_type=output_type, do_denormalize=do_denormalize)
+
+    # Offload all models
+    pipeline.maybe_free_model_hooks()
+
+    return StableDiffusionPipelineOutput(images=image, nsfw_content_detected=has_nsfw_concept)
